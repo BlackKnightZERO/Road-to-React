ea0ced1ada9d3f60acf4300846d225054a80fd7a
Arif Faysal <ayon.arif.10@gmail.com>
2020-11-04 21:16:49 +0600

    Form-Validation

--- src/ReactForm/SignUp.js
+++ src/ReactForm/SignUp.js
@@ -20,10 +20,14 @@ class SignUp extends Component {
         // alert(this.state);
         console.log(this.state);
         var container = document.getElementById('showValidationResult');
-        var element = `<h5>${this.state.fullName}</h5>
-                        <h5>${this.state.mobile}</h5>
-                        <h5>${this.state.email}</h5>
-                        <h5>${this.state.password}</h5>`;
+
+        var h5_1 = React.createElement('h5', {}, `${this.state.fullName}`);
+        var h5_2 = React.createElement('h5', {}, `${this.state.mobile}`);
+        var h5_3 = React.createElement('h5', {}, `${this.state.email}`);
+        var h5_4 = React.createElement('h5', {}, `${this.state.password}`);
+
+        var element = React.createElement('div', {}, [h5_1, h5_2, h5_3, h5_4]);
+        
         ReactDOM.render(element,container);
     }
 
@@ -36,6 +40,38 @@ class SignUp extends Component {
                 [inputName] : inputValue,
             }
         );
+        //validation    
+        if(inputName === 'mobile'){
+            if(!Number(inputValue)){
+                if(inputValue.length != 11){
+                    this.setState(
+                        {
+                            mobile: "Enter a valid phone no",
+                        }
+                    )
+                } 
+            }
+        }
+        if(inputName === 'email'){
+            var emailPatter = /\S+@\S+\.\S+/;
+            if(emailPatter.test(inputValue)){
+                this.setState(
+                    {
+                        mobile: "Enter a valid Email",
+                    }
+                )
+            } 
+        }
+        if(inputName === 'password'){
+            var passPattern = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[a-zA-Z]).{8,}$/;
+            if(!passPattern.test(inputValue)){
+                this.setState(
+                    {
+                        password : "Password Requirement Failed!",
+                    }
+                );
+            }
+        }
     }
 
 
@@ -43,21 +79,22 @@ class SignUp extends Component {
         return (
             <div>
                 <form onSubmit={this.formSubmitHandler}>
+                    <h5>Need to do on change event handler, not on submit</h5>
                     <div className="container w-25 mt-4">
                         <div className="form-group">
                             <label className="float-left"><b>Full Name</b></label>
                             <input type="text" className="form-control" name="fullName" id="fullName" placeholder="Enter Name" onChange={this.onChangeHandler}></input>
                         </div>
                         <div className="form-group">
-                            <label className="float-left"><b>Mobile</b></label>
-                            <input type="text" className="form-control" name="mobile" id="mobile" placeholder="+8801XXX" onChange={this.onChangeHandler}></input>
+                            <label className="float-left"><b>Mobile - 11 digits</b></label>
+                            <input type="text" className="form-control" name="mobile" id="mobile" placeholder="017..." onChange={this.onChangeHandler}></input>
                         </div>
                         <div className="form-group">
                             <label className="float-left"><b>Email address</b></label>
-                            <input type="email" className="form-control" name="email" id="email" placeholder="Enter email" onChange={this.onChangeHandler}></input>
+                            <input type="text" className="form-control" name="email" id="email" placeholder="Enter email" onChange={this.onChangeHandler}></input>
                         </div>
                         <div className="form-group">
-                            <label className="float-left"><b>Password</b></label>
+                            <label className="float-left"><b>Password - Testing193!</b></label>
                             <input type="password" className="form-control" name="password" id="password" placeholder="****" onChange={this.onChangeHandler}></input>
                         </div>
                         {/* <button type="submit" class="btn btn-success">Submit</button> */}
@@ -65,7 +102,7 @@ class SignUp extends Component {
                     </div>
                 </form>
 
-                <div className="bg-dark" id="showValidationResult">
+                <div className="bg-dark text-white" id="showValidationResult" style={{marginTop:'15px'}}>
 
                 </div>
 
